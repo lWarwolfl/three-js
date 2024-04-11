2fafbeee47c600d86c28d4c614fc6a5043dea2a9
Sina Kheiry <m.sinakheiry.warwolf@gmail.com>
2024-04-11 18:45:06 +0330

    Finished transformations session

--- 2-transform-objects/src/main.ts
+++ 2-transform-objects/src/main.ts
@@ -4,13 +4,28 @@ import "./styles/index.css";
 const scene = new THREE.Scene();
 
 const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: 0x5d5d5d });
-const mesh = new THREE.Mesh(geometry, material);
+const material1 = new THREE.MeshBasicMaterial({ color: 0x5d5d5d });
+const material2 = new THREE.MeshBasicMaterial({ color: 0x111111 });
+const material3 = new THREE.MeshBasicMaterial({ color: 0x333333 });
+const material4 = new THREE.MeshBasicMaterial({ color: 0x555555 });
 
-mesh.rotation.y = 10;
-mesh.rotation.x = 10;
+const cube1 = new THREE.Mesh(geometry, material1);
 
-scene.add(mesh);
+const cube2 = new THREE.Mesh(geometry, material2);
+cube2.position.x = 1.1;
+
+const cube3 = new THREE.Mesh(geometry, material3);
+cube3.position.y = 1.1;
+
+const cube4 = new THREE.Mesh(geometry, material4);
+cube4.position.z = 1.1;
+
+const group = new THREE.Group();
+group.add(cube1, cube2, cube3, cube4);
+
+group.rotation.y = -Math.PI / 6;
+
+const axesHelper = new THREE.AxesHelper(2);
 
 const camera = new THREE.PerspectiveCamera(
   75,
@@ -19,9 +34,11 @@ const camera = new THREE.PerspectiveCamera(
   1000
 );
 
+camera.position.y = 3;
 camera.position.z = 5;
+camera.lookAt(group.position);
 
-scene.add(camera);
+scene.add(camera, axesHelper, group);
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
