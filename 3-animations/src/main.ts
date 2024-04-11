834c2381fa2c44faa18511457256b96a5104e599
Sina Kheiry <m.sinakheiry.warwolf@gmail.com>
2024-04-11 19:38:40 +0330

    Finished animations session

--- 3-animations/src/main.ts
+++ 3-animations/src/main.ts
@@ -1,13 +1,14 @@
+import gsap from "gsap";
 import * as THREE from "three";
 import "./styles/index.css";
 
 const scene = new THREE.Scene();
 
 const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material1 = new THREE.MeshBasicMaterial({ color: 0x5d5d5d });
-const material2 = new THREE.MeshBasicMaterial({ color: 0x111111 });
-const material3 = new THREE.MeshBasicMaterial({ color: 0x333333 });
-const material4 = new THREE.MeshBasicMaterial({ color: 0x555555 });
+const material1 = new THREE.MeshBasicMaterial({ color: 0x333333 });
+const material2 = new THREE.MeshBasicMaterial({ color: 0x444444 });
+const material3 = new THREE.MeshBasicMaterial({ color: 0x555555 });
+const material4 = new THREE.MeshBasicMaterial({ color: 0x666666 });
 
 const cube1 = new THREE.Mesh(geometry, material1);
 
@@ -22,8 +23,6 @@ cube4.position.z = 1.1;
 
 const group = new THREE.Group();
 group.add(cube1, cube2, cube3, cube4);
-
-group.rotation.y = -Math.PI / 6;
 
 const axesHelper = new THREE.AxesHelper(2);
 
@@ -44,6 +43,33 @@ const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-renderer.render(scene, camera);
+// let time = Date.now();
 
-console.log("huh");
+// const tick = () => {
+//   const currentTime = Date.now();
+//   const deltaTime = currentTime - time;
+//   time = currentTime;
+
+//   group.rotation.y += 0.001 * deltaTime;
+//   renderer.render(scene, camera);
+
+//   window.requestAnimationFrame(tick);
+// };
+
+// tick();
+
+gsap.to(group.position, { duration: 1, delay: 1, y: 1 });
+gsap.to(group.position, { duration: 1, delay: 2, y: 0 });
+
+let clock = new THREE.Clock();
+
+const tick = () => {
+  const elapsedTime = clock.getElapsedTime();
+
+  group.rotation.y = elapsedTime / 1.5;
+  renderer.render(scene, camera);
+
+  window.requestAnimationFrame(tick);
+};
+
+tick();
