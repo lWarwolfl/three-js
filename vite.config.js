027eec98073bfa2e6f0bc663f401c38b0bade3af
Sina Kheiry <sinakheiri.dev@gmail.com>
2024-10-13 22:58:43 +0330

    build: configured vite to handle multiple pages

---
+++ vite.config.js
@@ -0,0 +1,18 @@
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: "index.html",
+        basicScene: "1-basic-scene.html",
+        transformObjects: "2-transform-objects.html",
+        animations: "3-animations.html",
+        cameras: "4-cameras.html",
+        fullscreenResize: "5-fullscreen-resize.html",
+        geometries: "6-geometries.html",
+        debugUi: "7-debug-ui.html",
+      },
+    },
+  },
+});
