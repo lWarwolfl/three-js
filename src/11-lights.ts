d14adc56c31b60ebbac2fa151e66ebb5091bc8b5
Sina Kheiry <sinakheiri.dev@gmail.com>
2024-11-17 20:35:55 +0330

    rafactor: put loading mechanism in one code

--- src/11-lights.ts
+++ src/11-lights.ts
@@ -5,42 +5,6 @@ import { RectAreaLightHelper } from "three/examples/jsm/helpers/RectAreaLightHel
 
 const gui = new dat.GUI();
 
-const loadingManger = new THREE.LoadingManager();
-
-document.body.classList.add("loading");
-const loadingContainer = document.getElementById("loading-container");
-const percentageContainer = document.getElementById("percentage");
-
-loadingManger.onProgress = (_, itemsLoaded, itemsTotal) => {
-  const progress = itemsLoaded / itemsTotal;
-  const percentage = parseFloat(progress.toFixed(2)) * 100;
-
-  if (percentageContainer)
-    percentageContainer.innerHTML = `${
-      percentage / 1 >= 1
-        ? percentage / 10 >= 1
-          ? percentage / 100 >= 1
-            ? percentage
-            : "0" + percentage
-          : "00" + percentage
-        : "000" + percentage
-    }%`;
-
-  if (progress === 1) {
-    document.body.classList.remove("loading");
-
-    if (loadingContainer) {
-      setTimeout(() => {
-        loadingContainer.style.opacity = "0";
-      }, 500);
-
-      setTimeout(() => {
-        loadingContainer.style.display = "none";
-      }, 1000);
-    }
-  }
-};
-
 const scene = new THREE.Scene();
 
 const standardMaterial = new THREE.MeshStandardMaterial({
